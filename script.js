82141277d432387e23aaeac5505540da47cf8b9f
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-07-18 08:29:40 +0200

    fix

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const menu = document.getElementById('menu');
   
     ref.addEventListener("click", function () {
-      if (clicked) {        
+      if (clicked) {     
         menu.style.display = 'flex';        
         menu.style.animation = 'fadeFromRight 0.4s ease-in-out';  
         ref.innerHTML = ' <i class="fas fa-times"></i>';        
         clicked = false;
-      } else {        
+      } else  {     
         menu.style.animationName = 'fadeToRight 0.4s ease-in-out';       
         ref.innerHTML = '<i class="fas fa-bars"></i>';
         clicked = true;
