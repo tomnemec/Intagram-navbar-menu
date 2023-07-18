a00c03638c5f676cc6ebbd362885d2b076d30950
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-07-18 08:06:34 +0200

    init

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var clicked = true;
+    var ref = document.getElementById("btn");
+    const menu = document.getElementById('menu');
+  
+    ref.addEventListener("click", function () {
+      if (clicked) {        
+        menu.style.display = 'flex';        
+        menu.style.animation = 'fadeFromRight 0.4s ease-in-out';  
+        ref.innerHTML = ' <i class="fas fa-times"></i>';        
+        clicked = false;
+      } else {        
+        menu.style.animationName = 'fadeToRight 0.4s ease-in-out';
+        menu.style.display = 'none';
+        ref.innerHTML = '<i class="fas fa-bars"></i>';
+        clicked = true;
+      }
+    });
+  });
+ 
+ 
