034df5d15d89338892439895995290cf4dba1df9
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-07-19 07:23:22 +0200

    fix

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var clicked = true;
-    var ref = document.getElementById("btn");
-    const menu = document.getElementById('menu');
-  
-    ref.addEventListener("click", function () {
-      if (clicked) {     
-        menu.style.display = 'flex';        
-        menu.style.animation = 'fadeFromRight 0.4s ease-in-out';  
-        ref.innerHTML = ' <i class="fas fa-times"></i>';        
-        clicked = false;
-      } else  {     
-        menu.style.animationName = 'fadeToRight 0.4s ease-in-out';       
-        ref.innerHTML = '<i class="fas fa-bars"></i>';
-        clicked = true;
-      }
-    });
+  var clicked = true;
+  var ref = document.getElementById("btn");
+  const menu = document.getElementById('menu');
+  ref.addEventListener("click", function () {
+    if (clicked) { 
+      clicked = false;
+      menu.classList.remove('hide');
+      menu.classList.remove('menu--fade-out');
+      menu.classList.add('show');
+      menu.classList.add('menu--fade-in');      
+      ref.innerHTML = ' <i class="fas fa-times"></i>';   
+    } else  {              
+      clicked = true;  
+      menu.classList.remove('menu--fade-in');    
+      menu.classList.add('menu--fade-out');   
+      menu.classList.add('hide');
+      ref.innerHTML = '<i class="fas fa-bars"></i>';
+    }
   });
- 
- 
+});
+
